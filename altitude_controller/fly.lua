9c9dc96b0c9c0fbe8e71d18923ef1ef27c20f950
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-05-25 08:13:19 -0400

    Switched to bang/bang controller when close to target; also increased frequency

--- altitude_controller/fly.lua
+++ altitude_controller/fly.lua
@@ -1,25 +1,26 @@
 local target_altitude = tonumber(...) or 60 -- Command line argument
+
 local gravity = aero.getGravity().y
+local altitude_sensor = peripheral.find("altitude_sensor")
 
 ---Whether the craft should apply thrust upward right now
 ---@param velocity number
 ---@return boolean
 local function shouldApplyThrust(velocity, target_altitude)
-    local _, altitude, _ = gps.locate()
+    local altitude = altitude_sensor.getHeight()
 
-    if altitude == nil then
-        print("No GPS station available")
-        return velocity < 0
-    else
-        if velocity < 0 and altitude < target_altitude then
-            return true
-        end
-        local apex_if_thrust_stopped = altitude + (velocity * velocity) / (-2 * gravity)
-        return apex_if_thrust_stopped < target_altitude
+    if math.abs(altitude - target_altitude) <= 7 then
+        return altitude < target_altitude
     end
+
+    if velocity < 0 and altitude < target_altitude then
+        return true
+    end
+    local apex_if_thrust_stopped = altitude + (velocity * velocity) / (-2 * gravity)
+    return apex_if_thrust_stopped < target_altitude
 end
 
-local DT = 1 / 10
+local DT = 1 / 20
 
 while true do
     local v = sublevel.getLinearVelocity().y
