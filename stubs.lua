5e9c619c55224096e54334cd5cb4cb764c044093
Iraxon <156947097+Iraxon@users.noreply.github.com>
2026-06-17 18:13:46 -0400

    Full Vector stub

--- stubs.lua
+++ stubs.lua
@@ -5,9 +5,22 @@
 ---@field x number
 ---@field y number
 ---@field z number
----@field length fun(Vector): number
----@field mul fun(Vector, number): Vector
+---@field add fun(self: Vector, other: Vector): Vector
+---@operator add(Vector): Vector
+---@field sub fun(self: Vector, other: Vector): Vector
+---@operator sub(Vector): Vector
+---@field mul fun(self: Vector, k: number): Vector
 ---@operator mul(number): Vector
+---@field div fun(self: Vector, k: number): Vector
+---@operator div(number): Vector
+---@operator unm(): Vector
+---@field dot fun(self: Vector, other: Vector): number
+---@field cross fun(self: Vector, other: Vector): Vector
+---@field length fun(self: Vector): number
+---@field normalize fun(): Vector
+---@field round fun(self: Vector, other: number|nil): Vector
+---@field tostring fun(self: Vector): string
+---@field equals fun(self: Vector, other: any): boolean
 local Vector = {}
 
 ---Radar track from Create Radars radar.getTracks() method
